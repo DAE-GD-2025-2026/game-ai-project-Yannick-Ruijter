71344119b3a8ddeaaa367c6316a7b94972c771f3
yannick ruijter <ruyter6969@gmail.com>
2026-02-09 10:34:25 +0100

    added seeking behaviour

--- Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.cpp
+++ Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.cpp
@@ -4,3 +4,13 @@
 //SEEK
 //*******
 // TODO: Do the Week01 assignment :^)
+
+SteeringOutput Seek::CalculateSteering(float DeltaT, ASteeringAgent & Agent)
+{
+	SteeringOutput Steering{};
+	
+	Steering.LinearVelocity = Target.Position - Agent.GetPosition();
+	//show a cool thing
+	//add debug rendering
+	return Steering;
+}
